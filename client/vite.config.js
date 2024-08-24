03a1aa3af52b86cffddc98a6fc34e1f631cd6a8e
omrawat23 <omrawat23@gmail.com>
2024-08-24 19:32:47 +0530

    data updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://guts-dun.vercel.app',
         changeOrigin: true,
         secure: false,
         ws: true,
