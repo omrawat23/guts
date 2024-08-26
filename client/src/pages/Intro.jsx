c0a23fb9b1bd457965ee94adc36c4f399c38d60d
omrawat23 <omrawat23@gmail.com>
2024-08-26 20:19:42 +0530

    data updated

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from '../../src/components/Header';
-import backgroundImage from "../assets/guts.png";
-
+import backgroundImage from '../assets/guts.png';
 
 function Intro() {
   return (
-        <div className="relative min-h-screen">
-          <Header />
-          <main className="w-full h-full">
-            <img src={backgroundImage} className="w-full h-auto object-cover" alt="Blog banner" />
-          </main>
-        </div>
+    <div className="relative min-h-screen flex flex-col">
+      {/* <Header /> */}
+      <main className="flex-grow">
+        <img
+          src={backgroundImage}
+          className="w-full h-auto object-cover md:h-full"
+          alt="Blog banner"
+        />
+      </main>
+    </div>
   );
 }
 
