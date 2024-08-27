0f523a3a00d01628d877d4f2949610ce86ee640a
omrawat23 <omrawat23@gmail.com>
2024-08-27 22:28:53 +0530

    data updated

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../src/components/Header';
-import backgroundImage from '../assets/be.png';
+import backgroundImageDesktop from '../assets/be.png';
+import backgroundImageMobile from '../assets/bee.jpeg'; // Assume this is your mobile image
 
 function Intro() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust this breakpoint as needed
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+
   return (
     <div className="relative min-h-screen flex flex-col">
       <main className="flex-grow">
         <img
-          src={backgroundImage}
+          src={isMobile ? backgroundImageMobile : backgroundImageDesktop}
           className="w-full h-screen object-cover"
           alt="Blog banner"
         />
