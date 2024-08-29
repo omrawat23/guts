c91adfea851e59b11d9ca1b06306f24af2788a39
omrawat23 <omrawat23@gmail.com>
2024-08-29 21:25:20 +0530

    DATA UPDATED

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // Vite configuration
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://guts-dun.vercel.app', // Your deployed backend URL
-        changeOrigin: true,
-        secure: true,
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            proxyReq.setHeader('cookie', req.headers.cookie || '');
-          });
-        },
-      },
-    },
-  },
+  plugins: [react()]
 });
