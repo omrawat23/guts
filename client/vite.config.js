2a455345163c20de014162b1ee2bd448bcb9fbaf
omrawat23 <omrawat23@gmail.com>
2024-09-06 09:29:36 +0530

    data updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://guts-dun.vercel.app',
+        target: 'https://guts-1.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
