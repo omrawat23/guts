4830b439b0af0f8c5eb69417c58e45a8770c63fa
omrawat23 <omrawat23@gmail.com>
2024-09-30 03:58:44 +0530

    frontend updated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ export default function CreatePost() {
     data.set('content', content);
     data.set('file', files[0]);
     ev.preventDefault();
-    const response = await fetch(`${apiBaseUrl}/post`, {
+    const response = await fetch(`${apiBaseUrl}/user/:userId/post`, {
       method: 'POST',
       body: data,
       credentials: 'include',
